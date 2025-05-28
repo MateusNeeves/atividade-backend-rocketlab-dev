b709ccfae30dbb08f18ae9b9ff16467143e20733
MateusNeeves <mateus1603@gmail.com>
2025-05-28 18:52:38 -0300

    feat: initial project setup with NestJS + TS + Prisma with SQLite

---
+++ prisma/migrations/20250528213141_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" REAL NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
